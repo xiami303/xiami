c6b783466788c87ae7f59e6ad4e7be8ae8185f5f
xiami303 <wuhongxia303@163.com>
2014-09-12 18:34:57 +0800

    Update bkmanager.py

--- core/bkmanager.py
+++ core/bkmanager.py
@@ -12,20 +12,14 @@ import inspect
 from xml.dom.minidom import Document
 #  import win32ui  # can be used only on windows
 import MySQLdb
-
+import zlib
 
 from bs4 import BeautifulSoup  #http://www.crummy.com/software/BeautifulSoup/bs3/documentation.zh.html#Quick%20Start
 
 class Application(Frame):
     def importBookmarks(self):
 	filename = tkFileDialog.askopenfilename(initialdir = 'E:/Python')	
-	fileHandler = open(filename, "w+")
-	content = fileHandler.read();
-	content = content.replace('</A>', '</A></DT>')
-	fileHandler.write(content)
-	
-	#writeIntoDB(filename)
-	writeCategory(filename)
+	writeIntoDB(filename)
 
     def createWidgets(self):
         self.importBtn = Button(self)
@@ -62,7 +56,8 @@ def writeIntoDB_(filename):
 	    db.rollback()	    
     db.close()
    
-	   
+   
+      
 	   
 def writeIntoDB(filename):
     soup = BeautifulSoup(open(filename, 'r').read(), from_encoding = "iso-8599-8")
@@ -70,38 +65,34 @@ def writeIntoDB(filename):
 
     db = MySQLdb.connect("localhost","root","root","bkmanager" )
     cursor = db.cursor()       
+    
     aLinks = soup.find_all("a")
     for i in aLinks:
-	text =  i.string
+	text =  i.string	
 	url = i.get("href")
-	sql = "insert into bookmark (text,url) values (%s, %s)"
+	sql = "insert into bookmark (text,url,category,hash) values (%s, %s, %s, %s)"
 	try:
-	    cursor.execute(sql,(text, url))
+	    hash = zlib.crc32(text)+ zlib.crc32(url)
+	    cursor.execute(sql,(text, url, 0, hash))
 	    db.commit()
 	except:
-	    db.rollback()	    
+	    db.rollback()
+       
+    categories = soup.find_all("h3")
+    for i in categories:
+	text = i.string
+	level = 1
+	sql = "insert into category (text, level, parent, hash) values (%s, %s, %s, %s)"
+	try:
+	    hash = zlib.crc32(text) + zlib.crc32('1')
+	    cursor.execute(sql,(text, level, 1, hash))
+	    db.commit()
+	except:
+	    db.rollback()
     db.close()
     
     
-def writeCategory(filename):
-    soup = BeautifulSoup(open(filename, 'r').read(), from_encoding = "iso-8599-8")
-    soup.prettify()
 
-    db = MySQLdb.connect("localhost","root","root","bkmanager" )
-    cursor = db.cursor()       
-    levelRoot = soup.findAll("h3")
-    for i in levelRoot:
-	text =  i.string
-	url = i.get("href")
-	
-	dl = i.next_sibling	
-	sql = "insert into bookmark (text,url) values (%s, %s)"
-	try:
-	    cursor.execute(sql,(text, url))
-	    db.commit()
-	except:
-	    db.rollback()	    
-    db.close()    
     
     
 root = Tkinter.Tk()
